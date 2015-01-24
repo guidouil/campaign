b9d1418dc250186d027ff162bdbc2bbc9735f25a
Guidouil <guidouil@gmail.com>
2015-01-24 23:12:06 +0100

    Accounts Entry Config update

--- client/campaign.js
+++ client/campaign.js
@@ -4,11 +4,11 @@ Meteor.startup(function () {
     dashboardRoute: '/admin',
     profileRoute: 'profile',
     passwordSignupFields: 'EMAIL_ONLY',
-    showSignupCode: true,
+    showSignupCode: false,
     showOtherLoginServices: true,      // Set to false to hide oauth login buttons on the signin/signup pages. Useful if you are using something like accounts-meld or want to oauth for api access
     extraSignUpFields: [{             // Add extra signup fields on the signup page
       field: "name",                           // The database property you want to store the data in
-      name: "This Will Be The Initial Value",  // An initial value for the field, if you want one
+      name: "",  // An initial value for the field, if you want one
       label: "Full Name",                      // The html lable for the field
       placeholder: "John Doe",                 // A placeholder for the field
       type: "text",                            // The type of field you want
